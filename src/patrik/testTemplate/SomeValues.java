d261c928621a98ae2ab8c7853f59778f8d9710a2
Patrik Hulthén <patrik.hulthen@live.com>
2018-04-29 22:40:57 +0200

    Commits after cleaning!

---
+++ src/patrik/testTemplate/SomeValues.java
@@ -0,0 +1,13 @@
+package patrik.testTemplate;
+
+public class SomeValues {
+    private String stringValue;
+    private Long longValue;
+    private Float floatValue;
+
+    public SomeValues(String stringValue, Long longValue, Float floatValue) {
+        this.stringValue = stringValue;
+        this.longValue = longValue;
+        this.floatValue = floatValue;
+    }
+}
